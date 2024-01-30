0a45142f3625bea987bedbd26c70561fe60c8959
Karl-Mercado <155978038+Karl-Mercado@users.noreply.github.com>
2024-01-30 13:51:52 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import prelimexam from './ConditionalRenderingChallenge/prelimexam.jsx'
+
+function App() {
+  return (
+    <div>
+      <prelimexam />
+      <h2>Things to Bring</h2>
+      <p>My Strawhat</p>
+      <p>Gum gum Fruit</p>
+      <p>The Red Vest</p>
+    </div>
+  );
+}
+export default App
---
+++ prelimexam.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function prelimexam(){
+    return(
+        <div>
+            <h2>Things to Bring</h2>
+            <p>My Strawhat</p>
+            <p>Gum gum Fruit</p>
+            <p>The Red Vest</p>
+        </div>
+    );
+}
+export default prelimexam
